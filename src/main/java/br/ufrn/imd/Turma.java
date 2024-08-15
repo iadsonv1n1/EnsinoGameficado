1de87c51206bd466a10c8cae530fa7a556475b44
iadson vincius <i.vini@hotmail.com>
2024-08-15 18:52:00 -0300

    implementacao do construtor de turma

---
+++ src/main/java/br/ufrn/imd/Turma.java
@@ -0,0 +1,18 @@
+package br.ufrn.imd;
+
+public class Turma {
+    private int codigo;
+    private String nome;
+
+    Turma(String nome){
+        this.nome = nome;
+        this.codigo = geradorDeCodigoAleatorio();
+    }
+
+    public int geradorDeCodigoAleatorio(){
+        int range = (1000 - 9999) +1;
+        int numeroAleatorio = (int) ((range * Math.random()) + 1000);
+
+        return numeroAleatorio;
+    }
+}
